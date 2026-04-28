d187cfd97177d3329295d64786af3e7122ce5301
rithik56 <rithik19901998@gmail.com>
2026-04-28 12:59:02 +0530

    changes in rideManager.cpp

--- uber/rideManager.cpp
+++ uber/rideManager.cpp
@@ -1,20 +1,23 @@
 #include "rideManager.hpp"
 
-RideManager* RideManager::rideManagerInstance = nullptr;
+RideManager *RideManager::rideManagerInstance = nullptr;
 mutex RideManager::mtx;
 
-unordered_map<string, Rider*> RideManager::riders;
-unordered_map<string, Driver*> RideManager::drivers;
-unordered_map<string, Ride*> RideManager::rides;
+unordered_map<string, Rider *> RideManager::riders;
+unordered_map<string, Driver *> RideManager::drivers;
+unordered_map<string, Ride *> RideManager::rides;
 
-RideManager::RideManager() {
-
+RideManager::RideManager()
+{
 }
 
-RideManager* RideManager::getRideManagerInstance() {
-    if (rideManagerInstance == nullptr) {
+RideManager *RideManager::getRideManagerInstance()
+{
+    if (rideManagerInstance == nullptr)
+    {
         mtx.lock();
-        if (rideManagerInstance == nullptr) {
+        if (rideManagerInstance == nullptr)
+        {
             rideManagerInstance = new RideManager();
         }
         mtx.unlock();
@@ -22,85 +25,104 @@ RideManager* RideManager::getRideManagerInstance() {
     return rideManagerInstance;
 }
 
-void RideManager::addRider(Rider* rider) {
+void RideManager::addRider(Rider *rider)
+{
     string riderId = rider->getId();
     rideManagerInstance->riders[riderId] = rider;
 }
 
-void RideManager::addDriver(Driver* driver) {
+void RideManager::addDriver(Driver *driver)
+{
     string driverId = driver->getId();
     rideManagerInstance->drivers[driverId] = driver;
 }
 
-string RideManager::getRiderTripId(string riderId, RIDE_STATUS rideStatus) {
-    for (auto it = rides.begin(); it != rides.end(); it++) {
-        if ((it->second)->getDriverId() == riderId && (it->second)->getRideStatus() == rideStatus) {
+string RideManager::getRiderTripId(string riderId, RIDE_STATUS rideStatus)
+{
+    for (auto it = rides.begin(); it != rides.end(); it++)
+    {
+        if ((it->second)->getDriverId() == riderId && (it->second)->getRideStatus() == rideStatus)
+        {
             return it->first;
         }
     }
     return "";
 }
 
-string RideManager::getDriverTripId(string driverId, RIDE_STATUS rideStatus) {
-    for (auto it = rides.begin(); it != rides.end(); it++) {
-        if ((it->second)->getDriverId() == driverId && (it->second)->getRideStatus() == rideStatus) {
+string RideManager::getDriverTripId(string driverId, RIDE_STATUS rideStatus)
+{
+    for (auto it = rides.begin(); it != rides.end(); it++)
+    {
+        if ((it->second)->getDriverId() == driverId && (it->second)->getRideStatus() == rideStatus)
+        {
             return it->first;
         }
     }
     return "";
 }
 
-void RideManager::cancelRide(string riderId, RIDE_STATUS rideStatus) {
+void RideManager::cancelRide(string riderId, RIDE_STATUS rideStatus)
+{
     string rideId = getRiderTripId(riderId, rideStatus);
     rideManagerInstance->rides[rideId]->setRideStatus(RIDE_STATUS::CANCELLED);
 }
 
-void RideManager::acceptRide(string driverId) {
+void RideManager::acceptRide(string driverId)
+{
     string rideId = getDriverTripId(driverId, RIDE_STATUS::ASSIGNED);
+    rideManagerInstance->drivers[driverId]->updateAvailability(false);
     rideManagerInstance->rides[rideId]->setRideStatus(RIDE_STATUS::ASSIGNED);
 }
 
-void RideManager::rejectRide(string driverId) {
+void RideManager::rejectRide(string driverId)
+{
     string rideId = getDriverTripId(driverId, RIDE_STATUS::NOT_ASSIGNED);
     rideManagerInstance->rides[rideId]->setRideStatus(RIDE_STATUS::CANCELLED);
 }
 
-void RideManager::startRide(string rideId) {
+void RideManager::startRide(string rideId)
+{
     rideManagerInstance->rides[rideId]->setRideStatus(RIDE_STATUS::STARTED);
 }
 
-void RideManager::completeRide(string rideId) {
+void RideManager::completeRide(string rideId)
+{
+    rideManagerInstance->rides[rideId]->getDriver()->updateAvailability(true);
     rideManagerInstance->rides[rideId]->setRideStatus(RIDE_STATUS::COMPLETED);
 }
 
-void RideManager::assignRide(Ride* ride) {
+void RideManager::assignRide(Ride *ride)
+{
 
     string rideId = ride->getRideId();
+    Location *pickupLocation = ride->getPickupLocation();
 
-    rideManagerInstance->rides[rideId] = ride;
+    Driver *nearestDriver = nullptr;
+    int minDistance = INT_MAX;
 
-    Location* pickupLocation = ride->getPickupLocation();
-    if (drivers.size() == 0) {
+    for (auto it = drivers.begin(); it != drivers.end(); it++)
+    {
+        Location *driverLocation = it->second->getCurrentLocation();
+        bool isAvailable = it->second->getIsAvailable();
+        if (isAvailable)
+        {
+            int currDistance = abs(pickupLocation->getLatitude() - driverLocation->getLatitude()) + abs(pickupLocation->getLongitude() - driverLocation->getLongitude());
+            if (currDistance < minDistance)
+            {
+                minDistance = currDistance;
+                nearestDriver = it->second;
+            }
+        }
+    }
+
+    if (nearestDriver == nullptr) {
         cout << "no drivers available at the moment" << endl;
         return;
     }
 
-    Driver* nearestDriver;
-    int minDistance = INT_MAX;
-
-    for (auto it = drivers.begin(); it != drivers.end(); it++) {
-        Location* driverLocation = it->second->getCurrentLocation();
-        int currDistance = abs(pickupLocation->getLatitude() - driverLocation->getLatitude()) + abs(pickupLocation->getLongitude() - driverLocation->getLongitude());
-        if (currDistance < minDistance) {
-            minDistance = currDistance;
-            nearestDriver = it->second;
-        }
-    }
+    rideManagerInstance->rides[rideId] = ride;
 
     ride->setDriver(nearestDriver);
     ride->setPayment(new Payment(ride->calculateFare()));
     ride->setRideStatus(RIDE_STATUS::ASSIGNED);
 }
-
-
-
